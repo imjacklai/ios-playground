0b5a575de7fea54148e86dce8bf08869e93fb75b
Jack <imjacklai@gmail.com>
2017-07-16 09:18:57 +0800

    Remove main storyboard and add git ignore file.

---
+++ Playground/HomeController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeController.swift
+//  Playground
+//
+//  Created by Jack Lai on 16/07/2017.
+//  Copyright © 2017 Jack Lai. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+}
